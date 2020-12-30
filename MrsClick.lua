cb8acf12bed2adfb0dbeec22f6375f136f0733a9
samueldcorbin <samueldcorbin@gmail.com>
2020-12-30 01:04:28 -0500

    Add checks for more interactable types

--- MrsClick.lua
+++ MrsClick.lua
@@ -20,13 +20,24 @@ WorldFrame:HookScript("OnMouseUp", function (self, button)
     end
 end)
 
+-- GUID is only easy way to distinguish pets/creatures/vehicles/etc.
+-- If this and UnitCreatureType == "Not specified" aren't enough, we'll probably have to examine tooltips
+local function guid_check(target)
+    local guid = UnitGUID("mouseover")
+    local first_dash_pos = strfind(guid, "-", 1, true)
+    local guid_type = strsub(guid, 1, first_dash_pos - 1)
+    return guid_type == "Vehicle" or guid_type == "GameObject"
+end
+
 mrsclick_frame:SetScript("OnEvent", function (self, event)
     if event == "CURSOR_UPDATE" then
         -- Unit had a special cursor (player can interact with it or autoattack it)
         if not UnitExists("mouseover") or -- allow right click for non-unit objects like portals or levers
            UnitIsDead("mouseover") or -- allow right click for lootable corpses
            not UnitCanAttack("player", "mouseover") or -- no right click for attackable units...
-           UnitIsUnit("mouseover", "target") then -- ...unless they're the player's target
+           UnitIsUnit("mouseover", "target") or -- ...unless they're the player's target
+           UnitCreatureType("mouseover") == "Not specified" or -- some doors, totems, etc. "exist" and are "attackable" even though the player can't target or attack them
+           guid_check("mouseover") then -- Some interactables (e.g., squirrel bombs) are "attackable" creatures, but get their interaction from being "Vehicles"
             interactable = true
         end
         mrsclick_frame:UnregisterEvent("CURSOR_UPDATE") -- If cursor is non-default, we get two CURSOR_UPDATE events, but we only need to check one of them
